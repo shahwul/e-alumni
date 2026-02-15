7307e3b1a6906c8551938fd4055e8883824bff5f
SentientCorn <radityanarotama0@gmail.com>
2026-02-16 05:07:07 +0700

    feat: timeGrain selection for time graph

--- components/sebaran/charts/TimeGraphChart.jsx
+++ components/sebaran/charts/TimeGraphChart.jsx
@@ -5,19 +5,24 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
-  Tooltip
+  Tooltip,
 } from "recharts";
 import { useAnalytics } from "@/hooks/useAnalytics";
 
+import QuerySelector from "../QuerySelector";
 import ChartCard from "../ChartCard";
 
-export default function TimeGraphChart({kab,
+import { GRAIN_OPTIONS } from "@/lib/constants";
+import { useState } from "react";
+
+export default function TimeGraphChart({
+  kab,
   kec,
   year,
-  timeGrain = "year", // "year" | "quarter" | "month"
   height = 300,
-  onExpand
+  onExpand,
 }) {
+  const [ timeGrain, setTimeGrain ] = useState("year");
   const { data, loading, error } = useAnalytics({
     metric: "alumni",
     kab,
@@ -26,12 +31,14 @@ export default function TimeGraphChart({kab,
     timeGrain,
   });
 
+  console.log(data);
+
   if (loading) {
     return (
       <ChartCard title="Tren Alumni" height={height} onExpand={onExpand}>
-      <div className="h-full flex items-center justify-center text-slate-400">
-        Loading…
-      </div>
+        <div className="h-full flex items-center justify-center text-slate-400">
+          Loading…
+        </div>
       </ChartCard>
     );
   }
@@ -39,29 +46,41 @@ export default function TimeGraphChart({kab,
   if (error || data.length === 0) {
     return (
       <ChartCard title="Tren Alumni" height={height}>
-      <div className="h-full flex items-center justify-center text-slate-400">
-        Data Kosong
-      </div>
+        <div className="h-full flex items-center justify-center text-slate-400">
+          Data Kosong
+        </div>
       </ChartCard>
     );
   }
 
   return (
-    <ChartCard title="Tren Alumni" height={height} onExpand={onExpand}>
-    <ResponsiveContainer width="100%" height={300}>
-      <LineChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" />
-        <YAxis />
-        <Tooltip />
-        <Line
-          type="monotone"
-          dataKey="value"
-          strokeWidth={2}
-          dot={{ r: 3 }}
+    <ChartCard height={height} onExpand={onExpand}>
+      <div className="px-4 pt-4 pb-2 flex items-center justify-between">
+        <h5 className="text-sm font-semibold text-slate-600">
+          Perbandingan Data PTK
+        </h5>
+
+        <QuerySelector
+          label="Metric"
+          value={timeGrain}
+          onChange={setTimeGrain}
+          options={GRAIN_OPTIONS}
         />
-      </LineChart>
-    </ResponsiveContainer>
+      </div>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="time" />
+          <YAxis />
+          <Tooltip />
+          <Line
+            type="monotone"
+            dataKey="value"
+            strokeWidth={2}
+            dot={{ r: 3 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </ChartCard>
   );
 }
