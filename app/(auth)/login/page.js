3d0594671ffa40c014dbc95522ab224b603092a3
SuryaAtmaja1 <suryagame12q@gmail.com>
2026-02-24 08:51:30 +0700

    fix: update logo in DashboardSidebar and improve layout in LoginPage

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { LoginForm } from "@/components/auth/login-form"
+import { LoginForm } from "@/components/auth/login-form";
 
 export default function LoginPage() {
   const [form, setForm] = useState({ username: "", password: "" });
@@ -30,41 +30,43 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex min-h-svh items-center justify-center bg-slate-50 p-4 md:p-10"> 
-          <div className="grid w-sm overflow-hidden rounded-[26px] bg-white shadow-sm lg:grid-cols-1">
-            <div className="flex flex-col gap-4 p-6 md:p-5">
-              <div className="flex justify-center gap-2 md:justify-center">
-                <a href="#" className="flex items-center gap-2 font-medium item-center text-center">
-                  <div className=" flex size-15 items-center justify-center">     
-                      <img
-                        src="favicon.ico"
-                        alt="Image"
-                      />
-                  </div>
-                  <div className="grid col-2 justify-items-start gap-y-0">
-                    <div className="hidden text-xl font-bold sm:block leading-none m-0">
-                    E-alumni
-                    </div>
-                    <div className="hidden text-l sm:block  m-0">
-                    BBGTK DIY
-                    </div>
-                  </div>
-                </a>    
+    <div className="flex min-h-svh items-center justify-center bg-slate-50 p-4 md:p-10">
+      <div className="grid w-sm overflow-hidden rounded-[26px] bg-white shadow-sm lg:grid-cols-1">
+        <div className="flex flex-col gap-4 p-6 md:p-5">
+          <div className="flex justify-center gap-2 md:justify-center">
+            <a
+              href="#"
+              className="flex items-center gap-2 font-medium item-center text-center"
+            >
+              <div className=" flex size-15 items-center justify-center">
+                <img
+                  src="/favicon.ico"
+                  alt="Logo"
+                  className="h-8 w-8 rounded-lg shrink-0 object-cover"
+                />
               </div>
-              <hr className="mx-auto border-t border-gray-300 mb-0 max-w-xs w-full" />
-              <div className="flex flex-col gap-1 relative">
-                  <h1 className="text-xl font-bold text-center">Login Akun</h1>
-                  <p className="text-muted-foreground text-sm text-center ">
-                    Silahkan login dengan akun anda.
-                  </p>
+              <div className="grid col-2 justify-items-start gap-y-0">
+                <div className="hidden text-xl font-bold sm:block leading-none m-0">
+                  E-alumni
                 </div>
-              <div className="flex flex-1 items-center justify-center">
-                <div className="w-full">
-                  <LoginForm />
-                </div>
+                <div className="hidden text-l sm:block  m-0">BBGTK DIY</div>
               </div>
+            </a>
+          </div>
+          <hr className="mx-auto border-t border-gray-300 mb-0 max-w-xs w-full" />
+          <div className="flex flex-col gap-1 relative">
+            <h1 className="text-xl font-bold text-center">Login Akun</h1>
+            <p className="text-muted-foreground text-sm text-center ">
+              Silahkan login dengan akun anda.
+            </p>
+          </div>
+          <div className="flex flex-1 items-center justify-center">
+            <div className="w-full">
+              <LoginForm />
             </div>
           </div>
         </div>
+      </div>
+    </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Montserrat, Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
+import { icon } from "leaflet";
 
 const source_sans_3 = Source_Sans_3({
   variable: "--font-source-sans-3",
@@ -17,12 +18,17 @@ const montserrat = Montserrat({
 export const metadata = {
   title: "E-Alumni",
   description: "Aplikasi untuk mempermudah pengelolaan data alumni BBGTK",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} ${source_sans_3.variable} antialiased`}>
+      <body
+        className={`${montserrat.variable} ${source_sans_3.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
